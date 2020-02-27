25f91146256257321df377a2da9c369538890825
James Nowecki <James.Nowecki@Nology.local>
2020-02-27 09:21:57 +0000

    classes created

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
-class Ship = {
+class Ship {
+    constructor(id, shipName, hitPoints, damagePerHit) {
+        this.id = id;
+        this.shipName = shipName;
+        this.hitPoints = hitPoints;
+        this.damagePerHit = damagePerHit;
+    }
+
+        takeDamage() {
+            this.hitpoints = this.hitPoints - damagePerHit;
+        }
+
+        die() {
+
+        }
+}
+
+class AttackShip extends Ship {
+    constructor(id) {
+        super(id, shipName, hitPoints, damagePerHit);
+        this.id = id;
+        this. shipName = "Attack Ship"
+        this.hitPoints = 45;
+        this.damagePerHit = 12;
+    }
+}
+
+class DefenceShip extends Ship {
+    constructor(id) {
+    super(id, shipName, hitPoints, damagePerHit);
+    this.id = id;
+    this. shipName = "Defence Ship"
+    this.hitPoints = 80;
+    this.damagePerHit = 10;
+    }
+}
+
+class MotherShip extends Ship {
+    constructor(id) {
+        super(id, shipName, hitPoints, damagePerHit);
+        this.id = id;
+        this. shipName = "Mother Ship"
+        this.hitPoints = 100;
+        this.damagePerHit = 9;
+        }
     
+        everythingDiesOnDeath() {
+
+        }
 }
