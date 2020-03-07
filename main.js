8feddc5bdc31726626df8fd327f0a289b9052428
James Nowecki <James.Nowecki@Nology.local>
2020-03-07 12:16:45 +0000

    refactoring mostly complete - need to work on ms death and game reset

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 class Ship {
-    constructor(id, shipName, hitPoints, damagePerHit) {
-        this.id = id;
+    constructor(shipName, hitPoints, damagePerHit) {
         this.shipName = shipName;
         this.hitPoints = hitPoints;
         this.damagePerHit = damagePerHit;
@@ -9,69 +8,81 @@ class Ship {
         takeDamage() {
             this.hitPoints = this.hitPoints - this.damagePerHit;
         }
+
+        die() {
+            this.hitPoints = 0;
+        }
 }
 
 class AttackShip extends Ship {
-    constructor(id) {
-        super(id, "Attack Ship", 45, 12);
-        this.id = id;
+    constructor() {
+        super("AttackShip", 45, 12);
     };
 };
 
 class DefenceShip extends Ship {
-    constructor(id) {
-    super(id, "Defence Ship", 80, 10);
-    this.id = id;
+    constructor() {
+    super("DefenceShip", 80, 10);
     };
 };
 
 class MotherShip extends Ship {
-    constructor(id) {
-        super(id, "Mother Ship", 100, 9);
-        this.id = id;
-        };
-    
-        everythingDiesOnDeath() {
-            if(this.hitPoints <= 0) {
-                alienArray.forEach(destroyShip);
-                alert("You slaughtered the peaceful aliens! They brought only treasures and hope, and were prescribed a hot lead pill");
-            };
+    constructor() {
+        super("MotherShip", 1, 9);
         };
 };
 
-const ms1 = new MotherShip("ms1");
-const ds1 = new DefenceShip("ds1");
-const ds2 = new DefenceShip("ds2");
-const ds3 = new DefenceShip("ds3");
-const ds4 = new DefenceShip("ds4");
-const ds5 = new DefenceShip("ds5");
-const as1 = new AttackShip("as1");
-const as2 = new AttackShip("as2");
-const as3 = new AttackShip("as3");
-const as4 = new AttackShip("as4");
-const as5 = new AttackShip("as5");
+const shipContainer = document.getElementById("prints-ships");
 
-const alienArray = [ms1, ds1, ds2, ds3, ds4, ds5, as1, as2, as3, as4, as5];
+const noOfMotherShips = 1;
+const noOfAttackShips = 5;
+const noOfDefenceShips = 5;
+const totalShips = noOfMotherShips + noOfAttackShips + noOfDefenceShips;
 
-const getRandomElement = (shipArray) => {
-    return shipArray[Math.floor(Math.random() * shipArray.length)]
-};
+let alienArray = [];
 
-const damageRandomShip = (aliveArray) => {
-    const randomShip = getRandomElement(aliveArray);
+for (let index = 0; index < totalShips; index++) {
+
+    let newMotherShip;
+    let newAttackShip;
+    let newDefenceShip;
+
+    if (index < noOfMotherShips) {
+        newMotherShip = new MotherShip();
+       alienArray.push(newMotherShip);
+    } else if (index <= noOfAttackShips) {
+        newAttackShip = new AttackShip(); 
+        alienArray.push(newAttackShip)
+    } else if (index <= totalShips) {
+        newDefenceShip = new DefenceShip();
+        alienArray.push(newDefenceShip);
+    }
+}
+
+const printTheHTML = () => {
+    shipContainer.innerHTML = "";
+
+    alienArray.forEach(ship => {
+        shipContainer.innerHTML += `<p>${ship.shipName}: ${ship.hitPoints}</p>`;
+    })
+}
+
+const damageRandomShip = () => {
+    const randomShipIndex = [Math.floor(Math.random() * alienArray.length)]
+    const randomShip = alienArray[randomShipIndex];
     randomShip.takeDamage();
+    if (randomShip.hitPoints <= 0 && randomShip.shipName === "MotherShip") {
+        alienArray.forEach(ship => ship.die());
+        alert("You slaughtered our friends from across the stars");
+    } else if (randomShip.hitPoints <= 0) {
+        alienArray.splice(randomShipIndex, 1);
+    }
 };
 
-const printTheHitPoints = (ship) => {
-    const shipID = ship.id;
-    const shipHP = ship.hitPoints;
-    const shipHTML = document.getElementById(shipID);
-    if (ship.hitPoints > 0) {
-    return shipHTML.innerHTML = shipHP;
-    } else {
-        return shipHTML.innerHTML = "FRIEND FROM ACROSS THE STARS DESTROYED!";
-    };
-};
+// const everythingDiesOnMSDeath = () => {
+//     // alienArray = [];
+//     alert("You slaughtered the friends from across the stars");
+// };
 
 const checkIfAlive = (ship) => {
     if (ship.hitPoints >= 1) {
@@ -79,15 +90,7 @@ const checkIfAlive = (ship) => {
     };
 };
 
-const destroyShip = (ship) => {
-    const shipID = ship.id;
-    const shipHTML = document.getElementById(shipID);
-    return shipHTML.innerHTML = "FRIEND FROM ACROSS THE STARS DESTROYED!";
-}
-
 const doDamageOnClick = () => {
-    const alienArrayOfAliveShips = alienArray.filter(checkIfAlive)
-    damageRandomShip(alienArrayOfAliveShips);
-    alienArray.forEach(printTheHitPoints);
-    ms1.everythingDiesOnDeath();
+    damageRandomShip(alienArray);
+    printTheHTML();
 };
